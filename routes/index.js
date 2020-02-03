2a1b5979bd6b0ff3169d797a42319156133a729f
HZCTony <hzc1033@smail.nchu.edu.tw>
2020-02-03 17:07:55 +0800

    update homepage design

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var sigin = require('../public/model/DataAccessObject/signin.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WatchBuy' });
+
+  var role = req.cookies.role;
+  var token = req.cookies.token;
+  if (!role || !token) {
+    res.render('index', { title: 'WatchBuy', loginStatus: 'none' });
+  } else {
+    sigin.personCookieCheck(role, token).then(loginStatus => {
+      console.log(loginStatus);
+      
+      res.render('index', { title: 'WatchBuy' ,  loginStatus: loginStatus });
+    });
+  };
+
+
+
 });
 
 module.exports = router;
