53e3f2c933cf44c7aaca56fe7647f297f28d33c6
HZCTony <hzc1033@smail.nchu.edu.tw>
2020-02-25 19:26:51 +0800

    bug test

--- routes/roomlist.js
+++ routes/roomlist.js
@@ -13,7 +13,8 @@ router.get('/', function (req, res, next) {
   liveroomlist.getAllRooms().then(rooms => {
     signin.personCookieCheck(role, token).then(loginStatus => {
       Logo.getLogoImgPath(loginStatus.role, loginStatus.email).then(logoPath => {
-        loginStatus.logo = logoPath.logo;
+        console.log('logoPath.logo in roomlist.js ==',logoPath.logo);
+        loginStatus['logo'] = logoPath.logo;
         res.render('roomlist', { title: title, loginStatus: loginStatus, rooms: JSON.stringify(rooms) });
       });
     }).catch(err =>{
